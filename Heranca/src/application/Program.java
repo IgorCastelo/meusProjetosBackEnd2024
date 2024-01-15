88016f217884f681b1e8b1a11002c9ec22c5c610
Igor Castelo <freitascasteloigor@gmail.com>
2024-01-15 18:52:55 -0300

    Esse projeto trata de praticar os fundamentos de Herança na POO

---
+++ Heranca/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import enttities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Account account = new Account();
+		System.out.println(account);
+
+	}
+
+}
