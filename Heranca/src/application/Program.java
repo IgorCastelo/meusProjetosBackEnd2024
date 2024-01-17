c3d18e5b79f856d09c9c215f322259e339b55c7d
Igor Castelo <freitascasteloigor@gmail.com>
2024-01-17 00:08:44 -0300

    Palavra super, anotacao @Overrride, sobreposicao, classes e metodos final

--- Heranca/src/application/Program.java
+++ Heranca/src/application/Program.java
@@ -10,10 +10,29 @@ public class Program {
 		Account acc = new Account(1001, "Alex", 0.0);
 		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 		
-		//UPCASTING
-		
+		//UPCASTING(É FAZER UMA SUBCLASSE VIRAR UMA SUPER CLASSE)
+	
 		Account acc1 = bacc;
+		Account acc2 =  new BusinessAccount(1003, "bob", 0.0, 200.0);
+		Account acc3 =  new SavingAccount(1004,"Anna", 0.0, 0.01);
+		
+	
+		//DOWNCASING(É FAZER UMA SUPERCLASSE VIRAR UMA SUBCLASSE))
+		BusinessAccount acc4 = (BusinessAccount)acc2;
+		acc4.loan(100.0);
 		
 		
+	//	BusinessAccount acc5 = (BusinessAccount)acc3;
+		
+		if(acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.loan(100.0);
+		}
+		
+		if (acc3 instanceof SavingAccount) {
+			SavingAccount acc5 = (SavingAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+ 		}
 	}
 }
